cc1e567cb75a6fc09e21af0ec393c23c53d64101
Antony Capper <acapper56@gmail.com>
2018-11-04 22:53:35 +0000

    Initial project setup

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	database: {
+		connection:
+			'mongodb+srv://user:password@databasename-p43km.mongodb.net/databasename?retryWrites=true'
+	}
+};
---
+++ routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+var config = require('../config');
+
+var MongoClient = require('mongodb').MongoClient;
+var uri = config.database.connection;
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	MongoClient.connect(
+		uri,
+		{ useNewUrlParser: true },
+		function(err, client) {
+			if (err) throw err;
+			var dbo = client.db('HonorsProject');
+			var query = { 'customer.lastname': 'Black' };
+			const collection = dbo
+				.collection('JobSheets')
+				.find(query)
+				.toArray(function(err, result) {
+					if (err) throw err;
+					console.log(result);
+					res.render('index', { title: 'Express', result });
+				});
+			client.close();
+		}
+	);
+});
+
+module.exports = router;
