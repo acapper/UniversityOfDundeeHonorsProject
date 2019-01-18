ae4f586cd4b79dfaf6de6893e3ddd65c8ed15a0b
Antony Capper <acapper56@gmail.com>
2019-01-18 16:21:28 +0000

    Updated Travis database connection test

--- test/travisdatabaseconnection.js
+++ test/travisdatabaseconnection.js
@@ -1,16 +1,39 @@
+const expect = require('chai').expect;
 const assert = require('chai').assert;
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = 'mongodb+srv://travis:tsest@127.0.0.1';
+
+const config = require('../config');
+let uri = null;
+if (!config.database.travis || config.database.travis == null) {
+	uri = config.database.connection;
+} else {
+	uri = 'mongodb+srv://travis:test@127.0.0.1';
+}
 
 describe('Travis MongoDB', function() {
-	it('Database connection should produce no errors', function() {
-		MongoClient.connect(
-			uri,
-			{ useNewUrlParser: true },
-
-			function(err, client) {
-				assert.isNull(err, 'Database connection success');
+	it('Database connection should produce no errors', function(done) {
+		let connectingDb = new Promise(function(resolve, reject) {
+			MongoClient.connect(
+				uri,
+				{ useNewUrlParser: true },
+				function(err, db) {
+					if (err) {
+						reject(err);
+					} else {
+						resolve(db);
+					}
+				}
+			);
+		});
+		connectingDb.then(
+			result => {
+				expect(result).not.to.equal(null);
+				done();
+				result.close();
+			},
+			reason => {
+				done(reason);
 			}
 		);
 	});
