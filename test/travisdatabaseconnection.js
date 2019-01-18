3a958bc9817b601930287ab7438d4abd8c0ea29e
Antony Capper <acapper56@gmail.com>
2019-01-18 15:32:47 +0000

    Added test for Travis MongoDB test database

---
+++ test/travisdatabaseconnection.js
@@ -0,0 +1,17 @@
+const assert = require('chai').assert;
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = 'mongodb+srv://travis:test@127.0.0.1';
+
+describe('Travis MongoDB', function() {
+	it('Database connection should produce no errors', function() {
+		MongoClient.connect(
+			uri,
+			{ useNewUrlParser: true },
+
+			function(err, client) {
+				assert.isNull(err, 'Database connection success');
+			}
+		);
+	});
+});
