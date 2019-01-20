ffd0a3eb20703ba63ff5882f915a3ed43bab1fe3
Antony Capper <acapper56@gmail.com>
2019-01-20 14:18:50 +0000

    Removed database calls from homepage

--- routes/index.js
+++ routes/index.js
@@ -8,47 +8,9 @@ var uri = config.database.connection;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	MongoClient.connect(
-		uri,
-		{ useNewUrlParser: true },
-
-		function(err, client) {
-			if (err) throw err;
-
-			var dbQuery1 = dbget.queryDatabase(
-				client,
-				'HonorsProject',
-				'JobSheets',
-				{
-					tags: 'parts'
-				}
-			);
-
-			var dbQuery2 = dbget.queryDatabase(
-				client,
-				'HonorsProject',
-				'JobSheets',
-				{
-					tags: 'site visit'
-				}
-			);
-
-			Promise.all([dbQuery1, dbQuery2])
-				.then(dbQueryR => {
-					res.render('index', {
-						title: 'OCS',
-						lists: [
-							{ tag: 'parts', jobsheets: dbQueryR[0] },
-							{ tag: 'site visit', jobsheets: dbQueryR[1] }
-						]
-					});
-					client.close();
-				})
-				.catch(err => {
-					throw err;
-				});
-		}
-	);
+	res.render('index', {
+		title: 'OCS'
+	});
 });
 
 module.exports = router;
