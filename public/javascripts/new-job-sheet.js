c0f8990705562207547b8edfa4d4bfe60f442d06
Antony Capper <acapper56@gmail.com>
2019-01-21 18:05:52 +0000

    Modified format of json submitted to database

--- public/javascripts/new-job-sheet.js
+++ public/javascripts/new-job-sheet.js
@@ -4,24 +4,44 @@ $('button').click(function() {
 	$('form#jobsheetform :input').each(function() {
 		var input = $(this);
 
+		var submit = true;
 		var data = new Object();
-		data.name = input.attr('name');
-		data.value = input.val();
 		data.type = input.attr('type');
+		data.name = input.attr('name');
+
+		if (data.type != 'radio') {
+			data.value = input.val();
+		}
+
 		if (data.type == 'checkbox') data.checked = input.is(':checked');
 
-		jobsheet.data[data.name] = data;
+		if (data.type == 'radio') {
+			if (input.is(':checked') == true) {
+				data.checked = input.is(':checked');
+				data.value = input.val();
+			} else {
+				submit = false;
+			}
+		}
+
+		if (data.type == 'date') data.value = new Date(data.value);
+
+		if (submit) jobsheet.data[data.name] = data;
 	});
 
+	console.log($('#id'));
 	console.log(jobsheet);
 
 	$.ajax({
 		type: 'POST',
 		url: '/',
 		dataType: 'json',
-		data: { jobsheet },
+		data: { id: $('#id').text(), jobsheet },
 		success: function() {
 			console.log('saved');
+			$('#savemessage').removeClass('uk-hidden');
+			var date = new Date().toLocaleTimeString();
+			$('#savemessage').text('Saved! ' + date);
 		}
 	});
 });
