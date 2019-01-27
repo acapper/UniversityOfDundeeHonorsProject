b44c75cd06b72a7234bc698a5b35754dc47dc5dd
Antony Capper <acapper56@gmail.com>
2019-01-27 21:14:58 +0000

    Updated config file to allow travis to use a different database

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 module.exports = {
 	database: {
 		connection:
-			'mongodb+srv://user:password@databasename-p43km.mongodb.net/databasename?retryWrites=true'
+			'mongodb+srv://user:password@databasename-p43km.mongodb.net/databasename?retryWrites=true',
+		travis: false,
+		name: 'HonorsProject'
 	}
 };
