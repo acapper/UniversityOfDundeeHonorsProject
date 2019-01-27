0a764c3b27566534ed36ba2dfb70603dd931d878
Antony Capper <acapper56@gmail.com>
2019-01-27 21:22:31 +0000

    Set tests to use travis database

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
 	database: {
 		connection:
 			'mongodb+srv://user:password@databasename-p43km.mongodb.net/databasename?retryWrites=true',
-		travis: false,
+		travis: true,
 		name: 'HonorsProject'
 	}
 };
