40bc83ef0ea99b09d61df3579519d15b5e709921
Antony Capper <acapper56@gmail.com>
2019-02-09 14:14:10 +0000

    Added data from parts list and site visit list to database

--- public/javascripts/job-sheet.js
+++ public/javascripts/job-sheet.js
@@ -28,6 +28,80 @@ $('.save-button').click(function() {
 		if (submit) jobsheet.data[name] = data;
 	});
 
+	var sitevisits = [];
+	$('.visit').each(function() {
+		var visit = new Object();
+		visit.data = {};
+		$(this)
+			.find(':input')
+			.each(function() {
+				var input = $(this);
+				var submit = true;
+				var data = new Object();
+				var type = input.attr('type');
+				var name = input.attr('name');
+				data.name = name;
+
+				if (type != 'radio' && type != 'checkbox')
+					data.value = input.val();
+
+				if (type == 'checkbox') data.checked = input.is(':checked');
+
+				if (type == 'radio') {
+					if (input.is(':checked') == true) {
+						data.checked = input.is(':checked');
+						data.value = input.val();
+					} else {
+						submit = false;
+					}
+				}
+
+				if (type == 'date') data.value = new Date(data.value);
+
+				if (submit) visit.data[name] = data;
+			});
+		sitevisits.push(visit);
+	});
+	jobsheet.data['sitevisits'] = sitevisits;
+
+	var parts = [];
+	$('.part').each(function() {
+		var part = new Object();
+		part.data = {};
+		$(this)
+			.find(':input')
+			.each(function() {
+				var input = $(this);
+				var submit = true;
+				var data = new Object();
+				var type = input.attr('type');
+				var name = input.attr('name');
+				data.name = name;
+
+				if (type != 'radio' && type != 'checkbox')
+					data.value = input.val();
+
+				if (type == 'checkbox') data.checked = input.is(':checked');
+
+				if (type == 'radio') {
+					if (input.is(':checked') == true) {
+						data.checked = input.is(':checked');
+						data.value = input.val();
+					} else {
+						submit = false;
+					}
+				}
+
+				if (type == 'date') data.value = new Date(data.value);
+
+				if (submit) part.data[name] = data;
+			});
+		parts.push(part);
+	});
+	jobsheet.data['parts'] = parts;
+
+	console.log(jobsheet);
+
 	$.ajax({
 		type: 'POST',
 		url: '/jobsheets/new',
@@ -51,6 +125,16 @@ $('.add-part').click(function() {
 		url: '/jobsheets/part',
 		success: function(res) {
 			$('#partlist').append(res);
+		}
+	});
+});
+
+$('.add-sitevisit').click(function() {
+	$.ajax({
+		type: 'GET',
+		url: '/jobsheets/sitevisit',
+		success: function(res) {
+			$('#sitelist').append(res);
 		}
 	});
 });
