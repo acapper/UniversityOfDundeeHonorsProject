3f7b2bea73a76123eb74adfc6532d4d7825bc0b2
Antony Capper <acapper56@gmail.com>
2019-02-09 11:33:54 +0000

    Modified layout to allow for parts

--- job-sheet-layout.js
+++ job-sheet-layout.js
@@ -69,29 +69,20 @@ module.exports = {
 			]
 		},
 		{
+			title: 'Parts',
+			name: 'parts',
+			fields: [
+				{
+					name: 'partadd',
+					label: 'Add Part',
+					type: 'partadd'
+				}
+			]
+		},
+		{
 			title: 'Order Details',
 			name: 'orderdetails',
 			fields: [
-				{ name: 'item', label: 'Item', type: 'text' },
-				{
-					name: 'date',
-					label: 'Date',
-					type: 'date'
-				},
-				{
-					name: 'ebay',
-					label: 'Ebay',
-					type: 'checkbox'
-				},
-				{
-					name: 'amazon',
-					label: 'Amazon',
-					type: 'checkbox'
-				},
-				{ name: 'other', label: 'Other', type: 'text' },
-				{ name: 'costprice', label: 'Cost Price', type: 'number' },
-				{ name: 'postage', label: 'Postage', type: 'number' },
-				{ name: 'saleprice', label: 'Sale Price', type: 'number' },
 				{
 					name: 'invoicenumber',
 					label: 'Invoice Number',
