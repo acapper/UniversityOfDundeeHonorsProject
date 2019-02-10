4197ceab77edaf0ae4c89b72d1874606ec714307
Antony Capper <acapper56@gmail.com>
2019-02-10 17:55:07 +0000

    Removed unused includes

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const config = require('../config');
-const layout = require('../job-sheet-layout');
-const db = require('../bin/database');
-const dbjobsheets = require('../bin/database-jobsheets');
-const mongo = require('mongodb');
-
-const uri = config.database.connection;
-const dbname = config.database.name;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
