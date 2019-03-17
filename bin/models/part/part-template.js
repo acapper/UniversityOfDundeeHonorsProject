65b11a6cb800447155fcb2bc7bfe59ef76c91a9f
Antony Capper <acapper56@gmail.com>
2019-03-17 23:45:54 +0000

    Created part template

---
+++ bin/models/part/part-template.js
@@ -0,0 +1,13 @@
+module.exports = {
+	status: { type: 'status', title: 'Status' },
+	name: { type: 'string', requiredfield: true, title: 'Name' },
+	purchased: { type: 'date', requiredfield: true, title: 'Purchased' },
+	store: { type: 'string', title: 'Bought from' },
+	cost: { type: 'number', requiredfield: true, title: 'Cost' },
+	postage: { type: 'number', title: 'Postage' },
+	markup: {
+		fixed: { type: 'number', title: 'Fixed Markup' },
+		percent: { type: 'number', title: 'Percent Markup' }
+	},
+	saleprice: { type: 'number', title: 'Sale Price' }
+};
