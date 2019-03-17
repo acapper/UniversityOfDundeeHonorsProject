720e14ef701f4de94ebe8a416631ed99c20b93eb
Antony Capper <acapper56@gmail.com>
2019-03-17 02:32:28 +0000

    Implemented part model

---
+++ bin/models/part/part.js
@@ -0,0 +1,57 @@
+const Part = require('../../db').Part;
+const utils = require('../../utlis');
+
+var exports = (module.exports = {});
+
+exports.new = part => {
+	return new Promise(function(resolve, reject) {
+		if (!part) throw new Error('Part is null');
+		if (!part.name) throw new Error('Name required');
+		if (!part.purchased) throw new Error('Purchase date required');
+		if (!part.cost) throw new Error('Cost required');
+
+		const data = new Part(part);
+		data.save(function(err, res) {
+			if (err) reject(err);
+			resolve(res);
+		});
+	});
+};
+
+exports.findOne = id => {
+	return new Promise(function(resolve, reject) {
+		utils.checkID(id);
+		Part.findById(id).exec((err, res) => {
+			if (err) reject(err);
+			resolve(res);
+		});
+	});
+};
+
+exports.update = (id, data) => {
+	return new Promise(function(resolve, reject) {
+		utils.checkID(id);
+		Part.findById(id, (err, doc) => {
+			if (err) reject(err);
+			if (!doc) {
+				reject(new Error('Document not found'));
+			} else {
+				doc = utils.updateJSON(doc, data);
+				doc.save(function(err, res) {
+					if (err) reject(err);
+					resolve(res);
+				});
+			}
+		});
+	});
+};
+
+exports.delete = id => {
+	return new Promise(function(resolve, reject) {
+		utils.checkID(id);
+		Part.findByIdAndRemove(id).exec((err, res) => {
+			if (err) reject(err);
+			resolve(res);
+		});
+	});
+};
