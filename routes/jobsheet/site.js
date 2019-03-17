4d848fbeab4683cb7562f5f89c7535498487ab47
Antony Capper <acapper56@gmail.com>
2019-03-17 02:34:36 +0000

    Started refactoring site routes

---
+++ routes/jobsheet/site.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const router = express.Router();
+const site = require('../../bin/models/site/site');
+
+router.get('/insert', function(req, res, next) {
+	var data = {
+		address: { street: 'street', city: 'city', postcode: 'postcode' },
+		date: Date.now(),
+		time: Date.now()
+	};
+	site.new(data)
+		.then(doc => {
+			res.json(doc);
+		})
+		.catch(err => {
+			res.send(err.message);
+		});
+});
+
+router.get('/:id', function(req, res, next) {
+	var id = req.params.id;
+	site.findOne(id)
+		.then(doc => {
+			res.json(doc);
+		})
+		.catch(err => {
+			res.send(err.message);
+		});
+});
+
+router.get('/update/:id', function(req, res, next) {
+	var id = req.params.id;
+	var data = { address: { city: 'dundee' } };
+	site.update(id, data)
+		.then(doc => {
+			res.json(doc);
+		})
+		.catch(err => {
+			res.send(err.message);
+		});
+});
+
+router.get('/delete/:id', function(req, res, next) {
+	var id = req.params.id;
+	site.delete(id)
+		.then(doc => {
+			res.json(doc);
+		})
+		.catch(err => {
+			res.send(err.message);
+		});
+});
+
+module.exports = router;
