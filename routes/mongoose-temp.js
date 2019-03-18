f0c3801bf1731358e638e14103b5bdac4de74e04
Antony Capper <acapper56@gmail.com>
2019-03-18 00:24:16 +0000

    Made all jobsheet route

--- routes/mongoose-temp.js
+++ routes/mongoose-temp.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const jobsheet = require('./jobsheet/jobsheet');
 const part = require('./jobsheet/part');
 const site = require('./jobsheet/site');
+const jobsheetModel = require('../bin/models/jobsheet/jobsheet');
 
 router.use('/jobsheet', jobsheet);
 router.use('/jobsheet/part', part);
@@ -11,6 +12,15 @@ router.use('/jobsheet/site', site);
 router.get('/', function(req, res, next) {
 	console.log('here');
 	res.send('got it');
+});
+
+router.get('/all', function(req, res, next) {
+	jobsheetModel.all().then(docs => {
+		res.render('mongoose/all', {
+			title: 'OCS',
+			jobsheets: docs
+		});
+	});
 });
 
 module.exports = router;
